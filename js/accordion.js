a094dae076dccfe9d850a08253a204ad9c58d66f
blazk <krapez87@gmail.com>
2019-03-02 11:45:19 +0100

    finished to json load

--- js/accordion.js
+++ js/accordion.js
@@ -1,11 +1,13 @@
 //wait till it loads
 window.addEventListener("load", function(event) {
   //get all accordions
-  var accordions = document.getElementsByClassName('accordion-example__accordion-title');
-  //loop thorugh
-  for (var i = 0; i < accordions.length; i++) {
+  var accordions = document.getElementsByClassName( 'accordion-example__accordion-title' );
+  //loop thorugh accordions
+  for ( var i = 0; i < accordions.length; i++ ) {
       accordions[i].addEventListener('click', function() {
-        this.classList.toggle('accordion-example__accordion-title--active');
+        //toggle class on click
+        this.classList.toggle( 'accordion-example__accordion-title--active' );
+        this.nextElementSibling.classList.toggle( 'accordion-example__accordion-panel--active' ); 
       });
   }
 
