b2601b779a6c78207c268bd4982baaaa0d09abb2
blazk <krapez87@gmail.com>
2019-10-01 17:11:39 +0200

    Improved build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var del = require('del');
 var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var cssnano = require('gulp-cssnano');
+var uglify = require ('gulp-uglify')
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
@@ -8,7 +11,10 @@ gulp.task('deleteDistFolder', function() {
 
 gulp.task('usemin', ['deleteDistFolder'], function() {
   return gulp.src("./index.html")
-  .pipe(usemin())
+  .pipe(usemin({
+    css: [function() {return rev()}, function() {return cssnano()}],
+    js: [function() {return rev()}, function() {return uglify()}],
+  }))
   .pipe(gulp.dest("./dist"));
 })
 
